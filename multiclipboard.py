19902da4edff313fd5b22df457cefa3c8a66daf4
Zach Rickers <mzrickers@gmail.com>
2022-04-19 07:26:51 +0200

    added json file

--- multiclipboard.py
+++ multiclipboard.py
@@ -10,3 +10,14 @@ print(sys.argv)
 if len(sys.argv) == 2:
     command = sys.argv[1]
     print(command)
+
+    if command == "save":
+        print("save")
+    elif command == "load":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("Unknown command")
+else:
+    print("Please pass exactly one command")
