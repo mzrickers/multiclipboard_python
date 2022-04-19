810414d502efecbc002ce059527cf417ea27ecfb
Zach Rickers <mzrickers@gmail.com>
2022-04-19 07:40:57 +0200

    save is working

--- multiclipboard.py
+++ multiclipboard.py
@@ -7,28 +7,31 @@ SAVED_DATA = "clipboard.json"
 data = clipboard.paste()
 print(data)
 
-def save_items(filepath, data):
+def save_data(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
-def load_items(filepath):
-    with open(filepath, "r") as f:
-        data = json.load(f)
-        return data
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
 
-save_items("test.json", {"key": "value"})
+save_data("test.json", {"key": "value"})
 
 print(sys.argv)
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
-    data = load_items(SAVED_DATA)
+    data = load_data(SAVED_DATA)
 
     if command == "save":
         print("save")
         key = input("Enter a key: ")
         data[key] = clipboard.paste()
-        save_items(SAVED_DATA, data)
+        save_data(SAVED_DATA, data)
     elif command == "load":
         print("load")
     elif command == "list":
