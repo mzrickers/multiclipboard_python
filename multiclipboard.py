aec42a8414246c5e4855af23c00a325744888a40
Zach Rickers <mzrickers@gmail.com>
2022-04-19 07:37:09 +0200

    created save and load commands

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,6 +2,8 @@ import sys
 import clipboard
 import json
 
+SAVED_DATA = "clipboard.json"
+
 data = clipboard.paste()
 print(data)
 
@@ -9,16 +11,24 @@ def save_items(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
+def load_items(filepath):
+    with open(filepath, "r") as f:
+        data = json.load(f)
+        return data
+
 save_items("test.json", {"key": "value"})
 
 print(sys.argv)
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
-    print(command)
+    data = load_items(SAVED_DATA)
 
     if command == "save":
         print("save")
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_items(SAVED_DATA, data)
     elif command == "load":
         print("load")
     elif command == "list":
